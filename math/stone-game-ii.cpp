c81dcc178f9f1c33ad9216976664491f569a0e43
Vikram Choudhary <thevikramgodara@gmail.com>
2026-08-09 21:20:52 +0530

    LC-stone-game-ii [Runtime: N/A; Memory: N/A]

---
+++ math/stone-game-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int n;
+    vector<int> suffix;
+    vector<vector<int>> dp;
+
+    int solve(int i, int M) {
+        if (i >= n)
+            return 0;
+
+        if (dp[i][M] != -1)
+            return dp[i][M];
+
+        // Can take all remaining piles
+        if (i + 2 * M >= n)
+            return dp[i][M] = suffix[i];
+
+        int ans = 0;
+
+        for (int X = 1; X <= 2 * M; X++) {
+            // Current player gets total remaining
+            // minus opponent's best
+            ans = max(ans,
+                suffix[i] - solve(i + X, max(M, X)));
+        }
+
+        return dp[i][M] = ans;
+    }
+
+    int stoneGameII(vector<int>& piles) {
+        n = piles.size();
+
+        // Suffix sum
+        suffix.resize(n + 1, 0);
+
+        for (int i = n - 1; i >= 0; i--)
+            suffix[i] = suffix[i + 1] + piles[i];
+
+        dp.assign(n, vector<int>(n + 1, -1));
+
+        return solve(0, 1);
+    }
+};
