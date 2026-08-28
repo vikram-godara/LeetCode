feb04afd4495e633456e039d617cf53a0a79effb
Vikram Choudhary <thevikramgodara@gmail.com>
2026-08-28 18:36:57 +0530

    LC-lexicographically-smallest-palindromic-permutation-greater-than-target [Runtime: N/A; Memory: N/A]

--- enumeration/lexicographically-smallest-palindromic-permutation-greater-than-target.cpp
+++ enumeration/lexicographically-smallest-palindromic-permutation-greater-than-target.cpp
@@ -2,11 +2,14 @@ class Solution {
 public:
     string lexPalindromicPermutation(string s, string target) {
         int n = s.size();
+        int m = n / 2;
 
         vector<int> cnt(26);
+
         for (char c : s)
             cnt[c - 'a']++;
 
+        // Check palindrome possibility
         int odd = 0;
         char mid = 0;
 
@@ -20,26 +23,82 @@ public:
         if (odd > 1)
             return "";
 
-        // Build smallest first half
+        // Smallest possible first half
         string half;
+
         for (int i = 0; i < 26; i++)
             half += string(cnt[i] / 2, 'a' + i);
 
-        // Every permutation of half gives one palindrome.
-        do {
-            string res = half;
+        // Function to build palindrome
+        auto makePalindrome = [&](string left) {
+            string res = left;
 
             if (n % 2)
                 res += mid;
 
-            string rev = half;
-            reverse(rev.begin(), rev.end());
-            res += rev;
+            reverse(left.begin(), left.end());
+            res += left;
 
-            if (res > target)
-                return res;
+            return res;
+        };
 
-        } while (next_permutation(half.begin(), half.end()));
+        // IMPORTANT:
+        // If the smallest half itself gives answer, return it.
+        string smallest = makePalindrome(half);
+
+        if (smallest > target)
+            return smallest;
+
+        // Now find the smallest half > target's first half
+        for (int i = m - 1; i >= 0; i--) {
+
+            vector<int> rem(26);
+
+            for (int j = 0; j < 26; j++)
+                rem[j] = cnt[j] / 2;
+
+            // Match target[0 ... i-1]
+            bool ok = true;
+
+            for (int j = 0; j < i; j++) {
+                int x = target[j] - 'a';
+
+                if (rem[x] == 0) {
+                    ok = false;
+                    break;
+                }
+
+                rem[x]--;
+            }
+
+            if (!ok)
+                continue;
+
+            // At position i, choose smallest character
+            // strictly greater than target[i]
+            int x = target[i] - 'a';
+
+            for (int c = x + 1; c < 26; c++) {
+
+                if (rem[c] == 0)
+                    continue;
+
+                string left = target.substr(0, i);
+                left += char('a' + c);
+
+                rem[c]--;
+
+                // Fill rest with smallest characters
+                for (int d = 0; d < 26; d++) {
+                    while (rem[d] > 0) {
+                        left += char('a' + d);
+                        rem[d]--;
+                    }
+                }
+
+                return makePalindrome(left);
+            }
+        }
 
         return "";
     }
