fb5ba2efac1b3cafbc6856a38cf038c2591b7cf7
Vikram Choudhary <thevikramgodara@gmail.com>
2026-08-29 15:03:28 +0530

    LC-next-permutation [Runtime: N/A; Memory: N/A]

--- two-pointers/next-permutation.cpp
+++ two-pointers/next-permutation.cpp
@@ -22,12 +22,12 @@ public:
         // sort(nums.begin(),nums.end());
 
         int i = n-2;
-        while(i>=0 && nums[i]>nums[i+1]) i--;
+        while(i>=0 && nums[i]>=nums[i+1]) i--;
 
        
         if(i>=0){
             int j = n-1;
-            while(nums[j]<nums[i]) j--;
+            while(nums[j]<=nums[i]) j--;
             swap(nums[i],nums[j]);
         }
         reverse(nums.begin()+i+1,nums.end());
