87e24ad48ae16dcc4583d8fbfc8cab4fc86970bc
Vikram Choudhary <thevikramgodara@gmail.com>
2026-08-29 18:31:36 +0530

    LC-longest-consecutive-sequence [Runtime: N/A; Memory: N/A]

--- union-find/longest-consecutive-sequence.cpp
+++ union-find/longest-consecutive-sequence.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
+        int n = nums.size();
+        if(n==0) return 0;
+        if(n==1) return 1;
         unordered_map<int,int>mp;
         for(int x : nums) mp[x]++;
-
         int mx = INT_MIN;
         int cnt =0;
         for(int x : nums){
