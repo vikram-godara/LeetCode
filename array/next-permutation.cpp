642cc72905d0e595866e00229e46edf1131503f5
Vikram Choudhary <thevikramgodara@gmail.com>
2026-08-29 14:18:30 +0530

    LC-next-permutation [Runtime: N/A; Memory: N/A]

--- array/next-permutation.cpp
+++ array/next-permutation.cpp
@@ -1,8 +1,20 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
-        for(int x : nums)cout<<x<<" ";
-        cout<<endl;
+        int n = nums.size();
+        int i = 0;
+        int mx = *max_element(nums.begin(),nums.end());
+        if(nums[0]==mx){
+            sort(nums.begin(),nums.end());
+            for(int x : nums) cout<<x<<" ";
+            cout<<endl;
+            return;
+        }
+        else{
+            sort(nums.begin()+1,nums.end(),greater<int>());
+            for(int x : nums) cout<<x<<" ";
+            cout<<endl;
+            return;
+        }
     }
 };
