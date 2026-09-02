a5a50ae7cbbd1100043eeac2b56ec34432b52010
Vikram Choudhary <thevikramgodara@gmail.com>
2026-09-02 10:20:00 +0530

    LC-merge-intervals [Runtime: N/A; Memory: N/A]

--- array/merge-intervals.cpp
+++ array/merge-intervals.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>>ans;
-        int n = intervals.size();
-        vector<pair<int,int>>v;
+        int n  = intervals.size();
         if(n==1){
             return {{intervals[0][0],intervals[0][1]}};
         }
-        for(int i=0;i<n;i++){
-            v.push_back({intervals[i][0],intervals[i][1]});
-        }
-        sort(v.begin(),v.end());
-        for(int i=1;i<v.size();i++){
-            if((v[i].first<=v[i-1].second) &&(v[i-1].second<=v[i].second)){
-                ans.push_back({v[i-1].first,v[i].second});
+        sort(intervals.begin(),intervals.end());
+
+        vector<vector<int>>ans;
+
+        for(int i=1;i<n;i++){
+            if((intervals[i][0]<=intervals[i-1][1])&&(intervals[i][1]>=intervals[i-1][1])){
+                intervals[i][0] = intervals[i-1][0];
+                intervals[i][1] = max(intervals[i][1],intervals[i-1][1]);
+               
             }
-            else {
-                if(i==1){
-                    ans.push_back({v[0].first,v[1].second});
-                }
-                ans.push_back({v[i].first,v[i].second});
+            else{
+                
+                ans.push_back(intervals[i-1]);
             }
         }
-        
+        ans.push_back(intervals[n-1]);
         return ans;
     }
 };
