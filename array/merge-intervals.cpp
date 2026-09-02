6f5909b4e8fd79e6a2fef4ae6893cc74f59be2ad
Vikram Choudhary <thevikramgodara@gmail.com>
2026-09-02 09:52:37 +0530

    LC-merge-intervals [Runtime: N/A; Memory: N/A]

--- array/merge-intervals.cpp
+++ array/merge-intervals.cpp
@@ -4,6 +4,9 @@ public:
         vector<vector<int>>ans;
         int n = intervals.size();
         vector<pair<int,int>>v;
+        if(n==1){
+            return {{intervals[0][0],intervals[0][1]}};
+        }
         for(int i=0;i<n;i++){
             v.push_back({intervals[i][0],intervals[i][1]});
         }
