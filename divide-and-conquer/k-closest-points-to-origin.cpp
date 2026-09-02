fe8f2b222587c9f86e00bd9cb93260950b0655bb
Vikram Choudhary <thevikramgodara@gmail.com>
2026-09-02 23:37:39 +0530

    LC-k-closest-points-to-origin [Runtime: N/A; Memory: N/A]

---
+++ divide-and-conquer/k-closest-points-to-origin.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        vector<pair<pair<int,int>,int>>v;
+        int n = points.size();
+        for(auto x : points){
+            int a = x[0];
+            int b = x[1];
+            long long  d = ((1LL*a*a)+ (1LL*b*b));
+            v.push_back({{a,b},d});
+        }
+        sort(v.begin(),v.end(),[](auto &a ,auto &b){
+            return a.second<b.second;
+        });
+        vector<vector<int>>ans;
+        for(int i =0;i<v.size();i++){
+            if(i<k)ans.push_back({v[i].first.first,v[i].first.second});
+            else break;
+        }
+        return ans;
+    }
+};
