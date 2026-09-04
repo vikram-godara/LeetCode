4c0e8f7865df6f12f8a3d0af15c7ef6bcb2127ef
Vikram Choudhary <thevikramgodara@gmail.com>
2026-09-04 10:44:59 +0530

    LC-smallest-stable-index-i [Runtime: N/A; Memory: N/A]

--- array/smallest-stable-index-i.cpp
+++ array/smallest-stable-index-i.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int>p(n),s(n);
-        int stable =INT_MAX;
-        p[0]=nums[0];
-        for(int i=1;i<n;i++){
-            // int max = *max_element(arr.begin(),arr.begin()+i);
-            // int min = *min_element(arr.begin()+i,arr.end());
-            // int d = max-min;
-            p[i] = max(p[i-1],nums[i]);
-            
-            
+        int n= nums.size();
+        vector<int>mx(n),mn(n);
+
+        mx[0] = nums[0];
+        for(int i = 1;i<n;i++){
+            mx[i] = max(nums[i],mx[i-1]);
         }
-        s[n-1] = nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            s[i]=min(s[i+1],nums[i]);
+        mn[n-1] = nums[n-1];
+        for(int i = n-2;i>=0;i--){
+            mn[i] = min(nums[i],mn[i+1]);
         }
-        for(int i=0;i<n;i++){
-            if(p[i]-s[i]<=k)return i;
+        int idx = -1;
+        for(int i =0;i<n;i++){
+            int d = mx[i]-mn[i];
+            if(d<=k){
+                return i;
+            }
         }
         return -1;
     }
