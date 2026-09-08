eef58915872c5c53a3daf6c05b12f1e940203ff3
Vikram Choudhary <thevikramgodara@gmail.com>
2026-09-08 19:01:39 +0530

    LC-longest-palindromic-substring [Runtime: N/A; Memory: N/A]

--- dynamic-programming/longest-palindromic-substring.cpp
+++ dynamic-programming/longest-palindromic-substring.cpp
@@ -4,20 +4,33 @@ public:
         int n = s.size();
         int i=0,j=0;
         string res ;
+        vector<string>substring;
         for(int i=0;i<n;i++){
             string r = "";
             for(int j = i;j<n;j++){
                 r = r + s[j];
-                int l = r.size();
-                bool ok = true;
-                for(int k=0;k<l/2;k++){
-                    if(r[k]!=r[l-k-1])ok = false;
-                }
-                if(ok){
-                    if(r.size()>res.size()) res = r;
-                }
+                substring.push_back(r);
+                // int l = r.size();
+                // bool ok = true;
+                // for(int k=0;k<l/2;k++){
+                //     if(r[k]!=r[l-k-1])ok = false;
+                // }
+                // if(ok){
+                //     if(r.size()>res.size()) res = r;
+                // }
             }
         }
+        for(int i=0;i<substring.size();i++){
+            int l = substring[i].size();
+            bool ok = true;
+            for(int k=0;k<l/2;k++){
+               if(substring[i][k]!=substring[i][l-k-1])ok = false;
+            }
+            if(ok){
+               if(substring[i].size()>res.size()) res = substring[i];
+            }
+        }
+        
         return res;
     }
 };
