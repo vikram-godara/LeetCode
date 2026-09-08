90c4ac1bfb2e380c4ec2a4628621033fa697fee1
Vikram Choudhary <thevikramgodara@gmail.com>
2026-09-08 19:10:57 +0530

    LC-longest-palindromic-substring [Runtime: N/A; Memory: N/A]

--- two-pointers/longest-palindromic-substring.cpp
+++ two-pointers/longest-palindromic-substring.cpp
@@ -2,35 +2,35 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n = s.size();
+        //TLE->>79/144 passed
+
         int i=0,j=0;
-        string res ;
-        vector<string>substring;
+        string res ="";
         for(int i=0;i<n;i++){
             string r = "";
             for(int j = i;j<n;j++){
                 r = r + s[j];
-                substring.push_back(r);
-                // int l = r.size();
-                // bool ok = true;
+                int l = r.size();
+                bool ok = true;
                 // for(int k=0;k<l/2;k++){
                 //     if(r[k]!=r[l-k-1])ok = false;
                 // }
-                // if(ok){
-                //     if(r.size()>res.size()) res = r;
-                // }
+                int a = 0,b= l-1;
+                while(a<b){
+                    if(r[a]!=r[b]) {
+                        ok = false;
+                        break;
+                    }
+                    a++;
+                    b--;
+                }
+                if(ok){
+                    if(r.size()>res.size()) res = r;
+                }
             }
         }
-        for(int i=0;i<substring.size();i++){
-            int l = substring[i].size();
-            bool ok = true;
-            for(int k=0;k<l/2;k++){
-               if(substring[i][k]!=substring[i][l-k-1])ok = false;
-            }
-            if(ok){
-               if(substring[i].size()>res.size()) res = substring[i];
-            }
-        }
-        
+
+
         return res;
     }
 };
