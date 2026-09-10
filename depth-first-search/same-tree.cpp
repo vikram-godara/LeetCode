e09663675ef061460206eae66c51edad1557186b
Vikram Choudhary <thevikramgodara@gmail.com>
2026-09-10 19:19:30 +0530

    LC-same-tree [Runtime: N/A; Memory: N/A]

---
+++ depth-first-search/same-tree.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool ans = true;
+    void solve(TreeNode* p, TreeNode* q){
+        if(p==nullptr && q==nullptr) return;
+
+        if(p==nullptr ||  q==nullptr || ans ==false) {
+            ans = false;
+            return;
+        }
+        if(p->val!=q->val){
+            ans= false;
+            return;
+        }
+        solve(p->left,q->left);
+        solve(p->right,q->right);
+
+    }
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        solve(p,q);
+        return ans;
+    }
+};
