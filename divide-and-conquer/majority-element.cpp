ae0833d106626e6a8442a97a38257ae9d846edf3
Vikram Choudhary <thevikramgodara@gmail.com>
2026-09-12 00:56:38 +0530

    LC-majority-element [Runtime: N/A; Memory: N/A]

--- divide-and-conquer/majority-element.cpp
+++ divide-and-conquer/majority-element.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int key =nums[0];
-        int maxcount =1;
-         int currCount =1;
         int n = nums.size();
-        for(int i=1;i<n;i++){
-            if(nums[i]==nums[i-1]){
-                currCount++;
-            }
-            else {
-                if(currCount>maxcount){
-                    maxcount = currCount;
-                    key = nums[i-1];
-                }
-                currCount =1;
-            }
+        int cnt =1;
+        int num = nums[0];   
+        for(int i = 1;i<n;i++){
+            if(num == nums[i]) cnt++;
+            else if(num!=nums[i]) cnt--;
+            
+            else if(cnt==0) num = nums[i];
         }
-        if(currCount>maxcount)key =nums[n-1];
-        return key ;
-        //return nums[nums.size() / 2]; -> this is for sort and then return size/2 element direct 
-    
+        return num;
     }
 };
