507747660d29cd19e84ef0002c4269f74aa5f837
Vikram Choudhary <thevikramgodara@gmail.com>
2026-09-13 14:55:09 +0530

    LC-image-overlap [Runtime: N/A; Memory: N/A]

--- array/image-overlap.cpp
+++ array/image-overlap.cpp
@@ -3,26 +3,44 @@ public:
     int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
         int n = img1.size();
         int ans = 0;
-        for(int dr = -(n-1) ; dr<n; dr++){
-            for(int dc = -(n-1) ; dc<n ;dc++){
+        // for(int dr = -(n-1) ; dr<n; dr++){
+        //     for(int dc = -(n-1) ; dc<n ;dc++){
 
 
-                int cnt =0 ;
+        //         int cnt =0 ;
 
-                for(int i =0;i<n;i++){
-                    for(int j= 0;j<n;j++){
-                        if(img1[i][j]==0) continue;
+        //         for(int i =0;i<n;i++){
+        //             for(int j= 0;j<n;j++){
+        //                 if(img1[i][j]==0) continue;
 
-                        int r = i+dr;
-                        int c = j+dc;
-                        if(r>=0 && r<n && c>=0 && c<n){
-                            if(img2[r][c]) cnt++;
-                        }
+        //                 int r = i+dr;
+        //                 int c = j+dc;
+        //                 if(r>=0 && r<n && c>=0 && c<n){
+        //                     if(img2[r][c]) cnt++;
+        //                 }
 
-                    }
-                }
+        //             }
+        //         }
                 
-                ans = max(ans,cnt);
+        //         ans = max(ans,cnt);
+        //     }
+        // }
+        // return ans;
+
+        vector<pair<int,int>>a,b;
+        for(int i = 0;i<n;i++){
+            for(int j=0;j<n;j++){
+               if(img1[i][j]) a.push_back({i,j});
+               if(img2[i][j]) b.push_back({i,j});
+            }
+        }
+        map<pair<int,int>,int>mp;
+        for(auto &[x1,y1]: a){
+            for(auto &[x2,y2]:b){
+                int x = x2-x1;
+                int y = y2-y1;
+                mp[{x,y}]++;
+                ans = max(ans,mp[{x,y}]);
             }
         }
         return ans;
