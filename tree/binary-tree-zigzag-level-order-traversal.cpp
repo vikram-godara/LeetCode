077ebe99de511eb1e456755c62def136c9cd5a20
Vikram Choudhary <thevikramgodara@gmail.com>
2026-09-14 12:17:42 +0530

    LC-binary-tree-zigzag-level-order-traversal [Runtime: N/A; Memory: N/A]

--- tree/binary-tree-zigzag-level-order-traversal.cpp
+++ tree/binary-tree-zigzag-level-order-traversal.cpp
@@ -6,44 +6,37 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
-        if (root == nullptr)
-            return ans;
-        queue<TreeNode*> q;
+        vector<vector<int>>ans;
+        if(root==nullptr) return ans;
+        queue<TreeNode*>q;
         q.push(root);
-        int cnt = 0;
-        while (!q.empty()) {
-            vector<int> arr;
+        int cnt=0;
+        while(!q.empty()){
+            vector<int>arr;
             int n = q.size();
-            for (int i = 0; i < n; i++) {
-                TreeNode* front = q.front();
+            for(int i=0;i<n;i++){
+                TreeNode*front = q.front();
                 q.pop();
                 arr.push_back(front->val);
-                if (cnt % 2 ==0) {
-                    if (front->right != nullptr) {
-                        q.push(front->right);
-                    }
-                    if (front->left != nullptr) {
-                        q.push(front->left);
-                    }
-                } else {
-                    if (front->left != nullptr) {
-                        q.push(front->left);
-                    }
-                    if (front->right != nullptr) {
-                        q.push(front->right);
-                    }
+                if(front->left!=nullptr){
+                    q.push(front->left);
+                }
+                if(front->right!=nullptr){
+                    q.push(front->right);
                 }
             }
+            if(cnt%2) {
+                reverse(arr.begin(),arr.end());
+                ans.push_back(arr);
+            }
+            else ans.push_back(arr);
             cnt++;
-            ans.push_back(arr);
         }
         return ans;
     }
