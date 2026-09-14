f4ef90f382ad4f8074703caf135cf1423a1a4ee2
Vikram Choudhary <thevikramgodara@gmail.com>
2026-09-14 12:14:49 +0530

    LC-binary-tree-zigzag-level-order-traversal [Runtime: N/A; Memory: N/A]

---
+++ breadth-first-search/binary-tree-zigzag-level-order-traversal.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if (root == nullptr)
+            return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        int cnt = 0;
+        while (!q.empty()) {
+            vector<int> arr;
+            int n = q.size();
+            for (int i = 0; i < n; i++) {
+                TreeNode* front = q.front();
+                q.pop();
+                arr.push_back(front->val);
+                if (cnt % 2 ==0) {
+                    if (front->right != nullptr) {
+                        q.push(front->right);
+                    }
+                    if (front->left != nullptr) {
+                        q.push(front->left);
+                    }
+                } else {
+                    if (front->left != nullptr) {
+                        q.push(front->left);
+                    }
+                    if (front->right != nullptr) {
+                        q.push(front->right);
+                    }
+                }
+            }
+            cnt++;
+            ans.push_back(arr);
+        }
+        return ans;
+    }
+};
