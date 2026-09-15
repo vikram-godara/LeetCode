48ec0a6bc888595fa31213f11d57a19a5b8c8ceb
Vikram Choudhary <thevikramgodara@gmail.com>
2026-09-16 02:52:06 +0530

    LC-rotate-array [Runtime: N/A; Memory: N/A]

--- math/rotate-array.cpp
+++ math/rotate-array.cpp
@@ -3,16 +3,17 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         int shift = k%n;
-        vector<int>ans;
-        for(int i = n-shift;i<n;i++){
-            ans.push_back(nums[i]);
-        }
-        for(int i=0;i<=n-shift-1;i++){
-            ans.push_back(nums[i]);
-        }
-        // for(int i=0;i<n;i++){
-        //     nums[i] = ans[i];
+        // vector<int>ans;
+        // for(int i = n-shift;i<n;i++){
+        //     ans.push_back(nums[i]);
         // }
-        nums = ans;
+        // for(int i=0;i<=n-shift-1;i++){
+        //     ans.push_back(nums[i]);
+        // }
+        // nums = ans;
+
+        reverse(nums.begin(),nums.begin()+n-shift);
+        reverse(nums.begin()+n-shift,nums.end());
+        reverse(nums.begin(),nums.end());
     }
 };
