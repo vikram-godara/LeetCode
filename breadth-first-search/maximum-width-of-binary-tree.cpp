1fc96a394231e7db8431de86a13c4618763eaa4f
Vikram Choudhary <thevikramgodara@gmail.com>
2026-09-16 12:32:54 +0530

    LC-maximum-width-of-binary-tree [Runtime: N/A; Memory: N/A]

--- breadth-first-search/maximum-width-of-binary-tree.cpp
+++ breadth-first-search/maximum-width-of-binary-tree.cpp
@@ -12,26 +12,26 @@
  */
 class Solution {
 public:
-    int mx = -1;
+    long long  mx = -1;
     void bfs(TreeNode* root) {
         if (root == NULL)
             return;
-        queue<pair<TreeNode*, int>> q;
+        queue<pair<TreeNode*,  long long  >> q;
         q.push({root, 0});
         while (!q.empty()) {
 
-            vector<int> arr;
             int n = q.size();
+            long long first = q.front().second;
+            long long last = first;
             for (int i = 0; i < n; i++) {
                 auto [f, v] = q.front();
                 q.pop();
-                arr.push_back(v);
-                if(f->left) q.push({f->left,2*v+1});
-                if(f->right) q.push({f->right,2*v+2});
+                v -= first;
+                last = v;
+                if(f->left) q.push({f->left,2*v});
+                if(f->right) q.push({f->right,2*v+1});
             }
-            int l = arr.size();
-            if(l==1) mx = max(mx,1);
-            else mx = max(mx,arr[l-1]-arr[0]+1);
+            mx = max(mx,last+1);
         }
     }
     int widthOfBinaryTree(TreeNode* root) {
