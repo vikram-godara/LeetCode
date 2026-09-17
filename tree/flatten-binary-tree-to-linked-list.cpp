08d50c199e4a6bb9b696346491d63e5cf765ed0b
Vikram Choudhary <thevikramgodara@gmail.com>
2026-09-18 00:52:54 +0530

    LC-flatten-binary-tree-to-linked-list [Runtime: N/A; Memory: N/A]

--- tree/flatten-binary-tree-to-linked-list.cpp
+++ tree/flatten-binary-tree-to-linked-list.cpp
@@ -6,27 +6,28 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    void dfs(TreeNode* root , vector<TreeNode*>&v) {
-        if(root == nullptr) return;
-        v.push_back(root);
-        dfs(root->left,v);
-        dfs(root->right,v);
-    }
     void flatten(TreeNode* root) {
-        if(root == nullptr) return;
-        vector<TreeNode*>v;
-        dfs(root,v);
+        TreeNode* cur = root;
 
-        for(int i =0;i<v.size()-1;i++){
-            v[i]->left = nullptr;
-            v[i]->right = v[i+1];
+        while (cur != NULL) {
+            if (cur->left) {
+                TreeNode* temp = cur->right;
+                cur->right = cur->left;
+                cur->left = NULL;
+                TreeNode*rightMost = cur->right;
+                while(rightMost->right!=nullptr){
+                    rightMost=rightMost->right;
+                }
+                rightMost->right = temp;
+
+            }
+            cur = cur->right;
         }
-        v.back()->left = nullptr;
-        v.back()->right = nullptr;
     }
 };
