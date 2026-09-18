b8766dec00f5a3e1c7172a2bebb66296401e4579
Vikram Choudhary <thevikramgodara@gmail.com>
2026-09-19 00:43:03 +0530

    LC-find-first-and-last-position-of-element-in-sorted-array [Runtime: N/A; Memory: N/A]

--- binary-search/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ binary-search/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,27 +2,14 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         int n = nums.size();
-        int l = 0,r = n-1;
-        int first=-1,last=-1;
-        while(l<=r){
-            int mid = r-(r-l)/2;
-            if(nums[mid] == target){
-                first = mid;
-                r= mid-1;
-            }
-            else if(nums[mid]>target) r= mid-1;
-            else l = mid+1;
-        }
-        l = 0,r = n-1;
-         while(l<=r){
-            int mid = r-(r-l)/2;
-            if(nums[mid] == target){
-                last = mid;
-                l = mid+1;
-            }
-            else if(nums[mid]>target) r= mid-1;
-            else l = mid+1;
-        }
-        return {first,last};
+        int lb = lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        if(lb ==n || nums[lb] != target) return {-1,-1};
+        int ub = upper_bound(nums.begin(),nums.end(),target)-nums.begin() -1;
+        // int l =0,h = n-1;
+        // while(l<=r){
+        //     int m = l + (r-l)/2;
+        //     if(nums[m]<=)
+        // }
+        return {lb,ub};
     }
 };
