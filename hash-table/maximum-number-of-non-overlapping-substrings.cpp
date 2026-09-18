d50b1d1f06fb5fb4ba4f6e7fe45175311ce468c5
Vikram Choudhary <thevikramgodara@gmail.com>
2026-09-18 21:54:12 +0530

    LC-maximum-number-of-non-overlapping-substrings [Runtime: N/A; Memory: N/A]

---
+++ hash-table/maximum-number-of-non-overlapping-substrings.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    vector<string> maxNumOfSubstrings(string s) {
+        int n = s.size();
+        unordered_map<char,pair<int,int>>mp;
+        for(int i=0;i<n;i++){
+            if(!mp.count(s[i])) mp[s[i]] = {i,i};
+            else{
+                // int l = mp[s[i]].first;
+                // int r = mp[s[i]].second;
+                // l = min(l,i);
+                // r = max(r,i);
+                // mp[s[i]] = {l,r};
+                mp[s[i]].second = i;
+            }
+        }
+        vector<string>substrings;
+        int j = 0;
+        while(j<n){
+            char a = s[j];
+            int l = mp[s[j]].first;
+            int r = mp[s[j]].second;
+            if(l==r) substrings.push_back(string(1,a));
+            else{
+                int len = r-l+1;
+                string sub = s.substr(l,len);
+                substrings.push_back(sub);
+            }
+            j++;
+        }
+        vector<pair<string,pair<int,int>>>finals;
+        for(int i =0;i<substrings.size();i++){
+            string sub = substrings[i];
+            int l = mp[sub[0]].first;
+            int r = mp[sub[0]].second;
+            bool ans = true;
+            for(int j=l;j<=r;j++){
+                int x = mp[sub[j]].first;
+                int y = mp[sub[j]].second;
+                if(x<l){
+                    ans = false;
+                    break;
+                }
+                r = max(r,y);
+            }
+            if(ans) finals.push_back({s.substr(l,r-l+1),{l,r}});
+        }
+        sort(finals.begin(),finals.end(), [](auto &a ,auto &b){
+            return a.second.second-a.second.first < b.second.second-b.second.first;
+        });
+        vector<pair<string,pair<int,int>>>selected;
+        for(auto &x : finals){
+            int l = x.second.frist;
+            int r = x.second.second;
+            bool overloop = false;
+            for(auto &y : selected){
+                int l2 = y.second.frist;
+                int r2 = y.second.second;
+                if(!(r<l2 || l>r2)){
+                    overlopp = true;
+                    break;
+                }
+            }
+            if(!overloop) selected.push_back();
+        }
+        vector<int>ans;
+        for(auto &x : selected){
+            ans.push_back(x.first);
+        }
+        return ans;
+    }
+};
