aff2c123dfbbfcf7b88496bf094adf2086752c34
Vikram Choudhary <thevikramgodara@gmail.com>
2026-09-19 15:56:12 +0530

    LC-search-in-rotated-sorted-array-ii [Runtime: N/A; Memory: N/A]

---
+++ binary-search/search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target)
+                return true;
+
+            if (nums[l] <= nums[mid]) {
+                {
+                    if (nums[l] <= target && nums[mid] >= target) {
+                        r = mid - 1;
+                    } else
+                        l = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && nums[r] >= target) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+};
