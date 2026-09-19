053218a6c82f08780d6e733f11a2271685996bb3
Vikram Choudhary <thevikramgodara@gmail.com>
2026-09-19 16:24:51 +0530

    LC-search-in-rotated-sorted-array-ii [Runtime: N/A; Memory: N/A]

--- array/search-in-rotated-sorted-array-ii.cpp
+++ array/search-in-rotated-sorted-array-ii.cpp
@@ -13,15 +13,15 @@ public:
                 r--;
                 continue;
             }
-            if (nums[mid] <= nums[r]) {
-                if (nums[mid] <= target && nums[r] <= target) {
-                    l = mid+1;
+            if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target && nums[mid] >= target) {
+                    r = mid-1;
                 }
-                else r = mid-1;
+                else l = mid+1;
             }
             else{
-                if(nums[l]<=target && nums[mid]>=target) r = mid-1;
-                else l = mid+1;
+                if(nums[mid]<=target && nums[r]>=target) l = mid+1;
+                else r = mid-1;
             }
         }
         return false;
