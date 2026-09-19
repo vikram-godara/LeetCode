c5831cd55df649a7c772e0342801c2281fa12a40
Vikram Choudhary <thevikramgodara@gmail.com>
2026-09-19 16:05:41 +0530

    LC-search-in-rotated-sorted-array-ii [Runtime: N/A; Memory: N/A]

--- binary-search/search-in-rotated-sorted-array-ii.cpp
+++ binary-search/search-in-rotated-sorted-array-ii.cpp
@@ -7,20 +7,15 @@ public:
             int mid = l + (r - l) / 2;
             if (nums[mid] == target)
                 return true;
-
-            if (nums[l] <= nums[mid]) {
-                {
-                    if (nums[l] <= target && nums[mid] >= target) {
-                        r = mid - 1;
-                    } else
-                        l = mid + 1;
+            if (nums[mid] <= nums[r]) {
+                if (nums[mid] <= target && nums[r] <= target) {
+                    l = mid+1;
                 }
-            } else {
-                if (nums[mid] <= target && nums[r] >= target) {
-                    l = mid + 1;
-                } else {
-                    r = mid - 1;
-                }
+                else r = mid-1;
+            }
+            else{
+                if(nums[l]<=target && nums[mid]>=target) r = mid-1;
+                else l = mid+1;
             }
         }
         return false;
